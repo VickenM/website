8911ef7e73d35b92adeb284c98dbda98c4ef7aeb
Vicken Mavlian <vicken.mavlian@gmail.com>
2024-12-05 23:16:54 -0500

    wip

--- cards.js
+++ cards.js
@@ -4,71 +4,44 @@ class Cards extends HTMLElement {
   constructor() {
     super();
 
-    const style = document.createElement("style");
-    style.textContent = ``;
-
-    const shadow = this.attachShadow({mode: "open"});
-
-    const container = document.createElement("article");
-    container.style.cssText = "display: flex; flex-direction: column;"; 
-
-    const header = document.createElement("div");
-    header.style.cssText = "display: flex;";
-
-    const heading = document.createElement("h2");
-    heading.style.cssText = "margin: 0rem; flex-grow: 1;"
-
-    const date = document.createElement("h4");
-    date.style.cssText = "margin: 0rem;"
-    date.textContent = "wicked"
-
-    const content = document.createElement("div");
-    content.id = "content";
-
-    header.appendChild(heading);
-    header.appendChild(date);
-    container.appendChild(header);
-    container.appendChild(content);
-
-    shadow.appendChild(style);
-    shadow.appendChild(container);
-    
-    this.container = container;
-    this.date = date;
-    this.heading = heading;
-  }
-
-  handlePrevious() {
-    this.selector.previous();
-  }
-
-  handleNext() {
-    this.selector.next();
+    this.innerHTML = `
+      <article 
+          class="${this.getAttribute('className')}" 
+          style="display: flex; flex-direction: column; gap: 1rem;"
+      >
+        <div style="display: flex;">
+          <h2 style="margin: 0rem; flex-grow: 1;">${this.getAttribute('heading')}</h2>
+          <h4 style="margin: 0rem;">${this.getAttribute('date')}</h4>
+        </div>
+        <div>
+          ${Array.from(this.children).map(element => `${element.innerHTML}`).join('')}
+        </div>
+      </article>
+    `;
   }
 
   disconnectedCallback() {
   }
 
   connectedCallback() {
-    if (document.readyState === "loading") {
-      document.addEventListener("DOMContentLoaded", function() {
-        const children = Array.from(this.children);
-        children.forEach((child)=>{this.container.appendChild(child)})
-      }.bind(this), {once: true})
-    } else {
-      const children = Array.from(this.children);
-      children.forEach((child)=>{this.container.appendChild(child)})
-    }
+//    if (document.readyState === "loading") {
+//      document.addEventListener("DOMContentLoaded", function() {
+//        const children = Array.from(this.children);
+//        children.forEach((child)=>{this.root.appendChild(child)})
+//      }.bind(this), {once: true})
+//    } else {
+//      const children = Array.from(this.children);
+//      children.forEach((child)=>{this.root.appendChild(child)})
+//    }
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name == "heading") {
-      this.heading.textContent = newValue;
-    } else if (name == "date") {
-      this.date.textContent = newValue;
-    } else if (name == "class") {
-      console.log(newValue)
-      this.container.classList.add(newValue.split(" "))
+      console.log(`new heading: ${oldValue} -> ${newValue}`)
+    } else if (name === "date") {
+      console.log(`new date: ${oldValue} -> ${newValue}`)
+    } else if (name === "class") {
+      console.log(`class list: ${newValue}`)
     }
   } 
 }
